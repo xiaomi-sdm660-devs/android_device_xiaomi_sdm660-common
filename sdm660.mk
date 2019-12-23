6793c71ea7d36aa4d659a473a9ddee3f71917485
PIPIPIG233666 <pig.priv@gmail.com>
2019-12-23 21:38:55 +0100

    sdm660-common: Include gapps
    Change-Id: Ib3581b439d1147f67162be7109b7c338613af188

--- sdm660.mk
+++ sdm660.mk
@@ -23,6 +23,9 @@
 
 # Inherit proprietary files
 $(call inherit-product, vendor/xiaomi/sdm660-common/sdm660-common-vendor.mk)
+ifdef WITH_GAPPS
+$(call inherit-product-if-exists, vendor/gapps/common/common-vendor.mk)
+endif
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
