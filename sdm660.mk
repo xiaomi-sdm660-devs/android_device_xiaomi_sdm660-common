7571c424042dc84187d64bec90af446f66f41d2a
dianlujitao <dianlujitao@lineageos.org>
2020-01-31 22:09:00 +0800

    hack
    Change-Id: I1df2c04543687c2ec73947adc012050b75084861

--- sdm660.mk
+++ sdm660.mk
@@ -25,7 +25,7 @@
 $(call inherit-product, vendor/xiaomi/sdm660-common/sdm660-common-vendor.mk)
 
 # Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Inherit properties
 $(call inherit-product, $(LOCAL_PATH)/properties.mk)
