377326d5a81b73ef05a6577a3c2d25694871e021
Erfan Abdi <erfangplus@gmail.com>
2020-03-07 05:30:13 +0330

    sdm660-common: Don’t enforce rro for all overlays
    * Breaking /product/overlay
    * Also this makes many platform specific overlays with current system API on vendor partition which breaks newer android version compatibilities (broken stuffs in gsis)
    
    Change-Id: I45eb18b0754726fb4f779521a0245dfcb1259b17

--- sdm660.mk
+++ sdm660.mk
@@ -33,11 +33,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
